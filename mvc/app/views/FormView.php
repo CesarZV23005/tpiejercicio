957360261749884927d1276e2e187e1387e06b14
CesarZV23005 <zv23005@ues.edu.sv>
2025-09-25 12:46:18 -0600

    commit

---
+++ mvc/app/views/FormView.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>formulario de informacion</h1>
+    <form method="post">
+        <label for="">Ingrese su correo</label>
+        <input type="text">
+        <br>
+        <br>
+        <label for="">Ingrese su numero</label>
+        <input type="text" name="" id="">
+        <br>
+        <br>
+        <label for="">ingrese su dui</label>
+        <input type="text">
+        <br>
+        <br>
+        <button type="submit">Registrar</button>
+    </form>
+</body>
+</html>
